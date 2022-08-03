40d1e847ba99c7d632fed567db58a0a05704c647
pptutn <48818730+pptutn@users.noreply.github.com>
2022-08-03 12:32:38 +1000

    Feature/reuseable box (#4)
    * made boxy
    
    * boxy with props
    
    * fuck this shit
    
    * boxes look kinda alright now
    
    * boxes are now clickable

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -2,15 +2,23 @@ import "./HomePage.css";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
+import InfoBox from "./InfoBox";
 
 function HomePage() {
   return (
-    <Stack spacing={{ xs: 1, sm: 2, md: 4 }} sx={{ width: 1, height: 1 }}>
+    <Stack
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+      sx={{ width: 1, height: 1 }}
+      alignItems={"centre"}
+      ml={2}
+      mr={2}
+    >
       <div>
         <h1 className="HomePage-title"> StuCom.</h1>
         <Autocomplete
           id="search-universities"
           freeSolo
+          sx={{ width: 0.9, height: 1 }}
           options={Universities.map((option) => option.name)}
           renderInput={(params) => (
             <TextField {...params} label="Search for your University" />
@@ -18,6 +26,20 @@ function HomePage() {
         />
         <h2 className="HomePage-title"> Frequently Searched Universities</h2>
       </div>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        alignItems={"centre"}
+        justifyContent={"space-evenly"}
+      >
+        <InfoBox name="Monash University" campusLocation="Clayton, VIC" />
+        <InfoBox
+          name="The University of Melbourne"
+          campusLocation="Parkville, VIC"
+        />
+        <InfoBox name="University of Sydney" campusLocation="Sydney, NSW" />
+        <InfoBox name="University of NSW" campusLocation="Kensington, NSW" />
+      </Stack>
     </Stack>
   );
 }
