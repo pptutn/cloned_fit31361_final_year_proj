e3a42fa6b27367101631c6fef1c5e35ffb6e83b8
ptra0010 <ptra0010@student.monash.edu>
2022-08-06 10:41:09 +1000

    use mui dialog box or make own

---
+++ src/Components/AuxComponents/InfoBox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import "./InfoBox.css";
+
+interface Props {
+  name?: string;
+  campusLocation?: string;
+}
+
+const showAlert = () => {
+  alert("God I love FIT3162");
+};
+
+function InfoBox(props: Props) {
+  return (
+    <Box
+      sx={{
+        width: 150,
+        height: 150,
+        backgroundColor: "#1AB65C",
+        "&:hover": {
+          backgroundColor: "#66bb6a",
+          opacity: [0.5, 0.5, 0.5],
+        },
+        borderRadius: 2,
+      }}
+      onClick={showAlert}
+    >
+      <Stack className={"Stack-stack"} ml={2} mr={2}>
+        <p className={"Box-title"}>{props.name}</p>
+        <p className={"Box-location"}>{props.campusLocation}</p>
+      </Stack>
+    </Box>
+  );
+}
+
+export default InfoBox;
