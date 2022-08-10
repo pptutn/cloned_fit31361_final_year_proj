77b98a44016c231ebc0f3dc02921ecc59478c7e6
philbertlan <85150276+philbertlan@users.noreply.github.com>
2022-08-10 10:57:40 +1000

    Added imports from SearchResults to App (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,23 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import LoginPage from "./Components/LoginPage";
 import SignupPage from "./Components/SignupPage";
-
-
+import SearchResults from "./Components/Results/searchResults";
+import SearchPage from "./Components/Results/searchPage";
 
 function App() {
-    const location = useLocation();
+  const location = useLocation();
 
+  return (
+    <div className="app">
+      <Routes location={location}>
+        <Route path="/" element={<HomePage />} />
 
-    return (
-        <div className="app">
-            
-            <Routes location ={location}>
-                <Route path='/' element={<HomePage />}/>
+        <Route path="/login" element={<LoginPage />} />
 
-                <Route path='/login' element={<LoginPage />}/>
-                    
-                <Route path='/signup' element={<SignupPage />}/>
-                
-            </Routes>
-        </div>
-    )
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
