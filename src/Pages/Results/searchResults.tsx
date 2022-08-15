38eb9d5f9e914073fae5c78dbdd5ffa7f8e84650
ptra0010 <ptra0010@student.monash.edu>
2022-08-15 16:54:46 +1000

    test monday

---
+++ src/Pages/Results/searchResults.tsx
@@ -0,0 +1,33 @@
+import "./searchResults.css";
+import FavoriteBorderIcon  from "@material-ui/icons/FavoriteBorder";
+
+function searchResults({
+  suburb,
+  price,
+  distance,
+  crime,
+  time
+}: any) {
+  return (
+    <div className = 'searchResult'>
+      <FavoriteBorderIcon className="searchResult__heart" />
+
+      <div className = 'resultsPageInfo'>
+        <div className='searchResult__info'>
+          <div className="searchResult__infoTop">
+              <h1>{suburb}</h1>
+          </div>
+          <div className='searchResult__infoBottom'>
+            <p>Average Rent: {price}</p>
+            <p>Distance: {distance}</p>
+            <p>Crime Rates: {crime}</p>
+            <p>Average Transit Time: {time}</p>
+            <p>Click here to search for rental houses</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default searchResults;
