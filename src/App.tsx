3005f548ca3685b0f79a493abfa7974ae11d5364
philbertlan <85150276+philbertlan@users.noreply.github.com>
2022-08-23 22:20:55 +1000

    Redirect search pages (#13)
    * Redirect search pages
    
    * Deleted searchResults files
    
    * Fixed positioning on hearts

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage";
 import LoginPage from "./Pages/LogIn/LoginPage";
 import SignupPage from "./Pages/SignUp/SignupPage";
-import SearchResults from "./Components/Results/searchResults";
-import SearchPage from "./Components/Results/searchPage";
-import SuburbDetails from "./Components/Suburb/SuburbDetails";
+import SearchResults from "./Pages/Results/searchResults";
+import SearchPage from "./Pages/Results/searchPage";
+import SuburbDetails from "./Pages/Suburb/SuburbDetails";
 
 function App() {
   const location = useLocation();
@@ -13,11 +13,15 @@ function App() {
   return (
     <div className="app">
       <Routes location={location}>
-        <Route path="/" element={<SuburbDetails />} />
+        <Route path="/" element={<HomePage />} />
 
         <Route path="/login" element={<LoginPage />} />
 
         <Route path="/signup" element={<SignupPage />} />
+
+        <Route path="/results" element={<SearchPage />} />
+
+        <Route path="/details" element={<SuburbDetails />} />
       </Routes>
     </div>
   );
