dec9b0e22f3bacafbdf36f52d62d1a7ef97ece40
ptra0010 <ptra0010@student.monash.edu>
2022-09-21 08:33:09 +1000

    fixed issue

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -41,88 +41,89 @@ function HomePage() {
 
   const handleSelection = (value: IFilterValues) => {
     setSelectedValue(value);
-    let navigate = useNavigate();
-    const showDetails = () => {
-      let path = `/details`;
-      navigate(path);
-    };
-
-    const showResults = () => {
-      let path = `/results`;
-      navigate(path);
-    };
-
-    return (
-      <Stack
-        spacing={{ xs: 1, sm: 2, md: 4 }}
-        sx={{ width: 1, height: 1 }}
-        alignItems={"centre"}
-        ml={2}
-        mr={2}
-      >
-        {/* POPUP STUFF */}
-        <div>
-          {/* <Typography variant="subtitle1" component="div">
-          Selected: {selectedValue}
-        </Typography> */}
-          <br />
-          <Button variant="outlined" onClick={handleClickOpen}>
-            FILTER MENU
-          </Button>
-          <SimpleDialog
-            selectedValue={selectedValue}
-            open={open}
-            handleSelection={handleSelection}
-            onClose={handleClose}
-          />
-        </div>
-        <div>MAX Distance {selectedValue.distance}</div>
-        <div>MAX Price {selectedValue.price}</div>
-        {/* POPUP STUFF */}
-        <div>
-          <Autocomplete
-            id="search-universities"
-            freeSolo
-            sx={{ width: 0.9, height: 1 }}
-            options={Universities.map((option) => option.name)}
-            renderInput={(params) => (
-              <TextField {...params} label="Search for your University" />
-            )}
-            onClick={showResults}
-            onChange={showResults}
-          />
-          <h2 className="HomePage-title"> Frequently Searched Universities</h2>
-        </div>
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          alignItems={"centre"}
-          justifyContent={"space-evenly"}
-        >
-          <InfoBox
-            name="Monash University"
-            campusLocation="Clayton, VIC"
-            onClick={showResults}
-          />
-          <InfoBox
-            name="The University of Melbourne"
-            campusLocation="Parkville, VIC"
-          />
-          <InfoBox name="University of Sydney" campusLocation="Sydney, NSW" />
-          <InfoBox name="University of NSW" campusLocation="Kensington, NSW" />
-        </Stack>
-      </Stack>
-    );
+  };
+  let navigate = useNavigate();
+  const showDetails = () => {
+    let path = `/details`;
+    navigate(path);
   };
 
-  const Universities = [
-    { name: "Monash University", state: "VIC" },
-    { name: "The University of Melbourne", state: "VIC" },
-    { name: "University of New South Wales", state: "NSW" },
-    { name: "University of Sydney", state: "NSW" },
-    { name: "Adelaide University", state: "SA" },
-    { name: "Queendsland Universty", state: "QLD" },
-    { name: "Royal Melbourne Insitute of Technology", state: "VIC" },
-  ];
+  const showResults = () => {
+    let path = `/results`;
+    navigate(path);
+  };
+
+  return (
+    <Stack
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+      sx={{ width: 1, height: 1 }}
+      alignItems={"centre"}
+      ml={2}
+      mr={2}
+    >
+      {/* POPUP STUFF */}
+      <div>
+        {/* <Typography variant="subtitle1" component="div">
+          Selected: {selectedValue}
+        </Typography> */}
+        <br />
+        <Button variant="outlined" onClick={handleClickOpen}>
+          FILTER MENU
+        </Button>
+        <SimpleDialog
+          selectedValue={selectedValue}
+          open={open}
+          handleSelection={handleSelection}
+          onClose={handleClose}
+        />
+      </div>
+      <div>MAX Distance {selectedValue.distance}</div>
+      <div>MAX Price {selectedValue.price}</div>
+      {/* POPUP STUFF */}
+      <div>
+        <Autocomplete
+          id="search-universities"
+          freeSolo
+          sx={{ width: 0.9, height: 1 }}
+          options={Universities.map((option) => option.name)}
+          renderInput={(params) => (
+            <TextField {...params} label="Search for your University" />
+          )}
+          onClick={showResults}
+          onChange={showResults}
+        />
+        <h2 className="HomePage-title"> Frequently Searched Universities</h2>
+      </div>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        alignItems={"centre"}
+        justifyContent={"space-evenly"}
+      >
+        <InfoBox
+          name="Monash University"
+          campusLocation="Clayton, VIC"
+          onClick={showResults}
+        />
+        <InfoBox
+          name="The University of Melbourne"
+          campusLocation="Parkville, VIC"
+        />
+        <InfoBox name="University of Sydney" campusLocation="Sydney, NSW" />
+        <InfoBox name="University of NSW" campusLocation="Kensington, NSW" />
+      </Stack>
+    </Stack>
+  );
 }
+
+const Universities = [
+  { name: "Monash University", state: "VIC" },
+  { name: "The University of Melbourne", state: "VIC" },
+  { name: "University of New South Wales", state: "NSW" },
+  { name: "University of Sydney", state: "NSW" },
+  { name: "Adelaide University", state: "SA" },
+  { name: "Queendsland Universty", state: "QLD" },
+  { name: "Royal Melbourne Insitute of Technology", state: "VIC" },
+];
+
 export default HomePage;
