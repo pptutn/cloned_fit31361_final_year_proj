911bab346d1c2b844db3bc340ded9cc9fbcb66ec
Philbert <plan0008@student.monash.edu>
2022-10-05 15:52:46 +1100

    Parts of rmit melbourne completed

--- src/Components/API/getSuburbData.ts
+++ src/Components/API/getSuburbData.ts
@@ -1,7 +1,9 @@
 const axios = require("axios").default;
 
 export async function getSearchData() {
-  const responseData = await axios.get("http://localhost:8080/api/suburbs");
+  const responseData = await axios.get(
+    "http://localhost:8080//suburbs/MON_CLA"
+  );
   return responseData.data;
 }
 
