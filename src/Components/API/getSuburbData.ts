131d622119533c418e6ed4eed1cc4c8919e3a7f1
Philbert <plan0008@student.monash.edu>
2022-10-05 16:37:07 +1100

    Created templates for all 8 universities

--- src/Components/API/getSuburbData.ts
+++ src/Components/API/getSuburbData.ts
@@ -1,9 +1,7 @@
 const axios = require("axios").default;
 
 export async function getSearchData() {
-  const responseData = await axios.get(
-    "http://localhost:8080//suburbs/MON_CLA"
-  );
+  const responseData = await axios.get("http://localhost:8080/suburbs/MON_CLA");
   return responseData.data;
 }
 
