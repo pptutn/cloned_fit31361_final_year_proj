18d7282a533cf58b4bc9e2759a93325255fd9920
ptra0010 <ptra0010@student.monash.edu>
2022-10-06 12:09:32 +1100

    adjusted scores to match actual model

--- src/Components/Algorithm/GetScore.tsx
+++ src/Components/Algorithm/GetScore.tsx
@@ -2,10 +2,13 @@ import { Subscript, SubscriptSharp } from "@mui/icons-material";
 import { suburbProps } from "../ExpandableComponent/ExpandableComponent";
 
 export const calcScore = (subs: suburbProps) => {
-  return Math.round(
-    subs.distance * 0.39 +
-      subs.medianRentPrice * 0.38 +
-      +(subs.bus || subs.train || subs.tram) * 0.14
+  return (
+    2 *
+    Math.round(
+      -subs.distance * 0.39 +
+        (500 - subs.medianRentPrice) * 0.47 +
+        +(subs.bus || subs.train || subs.tram) * 0.14
+    )
   );
 };
 
