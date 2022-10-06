efa2746e1e7a03c53ffd1668ab4e3d11438c710f
Philbert <plan0008@student.monash.edu>
2022-10-06 21:46:50 +1100

    added links in suburbdata.ts

--- src/Components/API/getSuburbData.ts
+++ src/Components/API/getSuburbData.ts
@@ -3,6 +3,14 @@ const axios = require("axios").default;
 export async function getSearchData() {
   // monash clayton
   const responseData = await axios.get("http://localhost:8080/suburbs/MON_CLA");
+  // latrobe bundoora
+  // const responseData = await axios.get("http://localhost:8080/suburbs/LAT_BUN");
+  // // victoria uni footscray
+  // const responseData = await axios.get("http://localhost:8080/suburbs/VIC_FOO");
+  // // swinburne hawthorn
+  // const responseData = await axios.get("http://localhost:8080/suburbs/SWI_HAW");
+  // // rmit melbourne
+  // const responseData = await axios.get("http://localhost:8080/suburbs/RMI_MEL");
   return responseData.data;
 }
 
