169022aab4b96c5b81d1379cadfa47b37d4e9437
ptra0010 <ptra0010@student.monash.edu>
2022-10-11 15:57:06 +1100

    small change with number

--- src/Components/Algorithm/GetScore.tsx
+++ src/Components/Algorithm/GetScore.tsx
@@ -5,7 +5,7 @@ export const calcScore = (subs: suburbProps) => {
   const res = Math.round(
     -subs.distance * 0.5 +
       (800 - subs.medianRentPrice) * 0.47 +
-      +(subs.bus || subs.train || subs.tram) * 0.14
+      +(subs.bus || subs.train || subs.tram) * 0.03
   );
   console.log("this is res", res);
   return res > 100 ? 100 : res < 0 ? 0 : res;
