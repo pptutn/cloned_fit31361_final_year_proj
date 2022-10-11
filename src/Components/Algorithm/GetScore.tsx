e851248e828deb5c686dfefb1cfb42b568280e16
ptra0010 <ptra0010@student.monash.edu>
2022-10-11 15:54:55 +1100

    fixed scoring, most suburbs are 100

--- src/Components/Algorithm/GetScore.tsx
+++ src/Components/Algorithm/GetScore.tsx
@@ -3,10 +3,11 @@ import { suburbProps } from "../ExpandableComponent/ExpandableComponent";
 
 export const calcScore = (subs: suburbProps) => {
   const res = Math.round(
-    -subs.distance * 0.39 +
-      (600 - subs.medianRentPrice) * 0.47 +
+    -subs.distance * 0.5 +
+      (800 - subs.medianRentPrice) * 0.47 +
       +(subs.bus || subs.train || subs.tram) * 0.14
   );
+  console.log("this is res", res);
   return res > 100 ? 100 : res < 0 ? 0 : res;
 };
 
