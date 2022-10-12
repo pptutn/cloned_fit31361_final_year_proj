f4cb086ca38bce8ce5893c3515b24886ea49d5bf
pptutn <48818730+pptutn@users.noreply.github.com>
2022-10-13 10:52:09 +1100

    fixed scoring, just adjusted number so that at least some are not 0 (#27)
    * fixed scoring, most suburbs are 100
    
    * small change with number
    
    * converted to stars rating

--- src/Components/Algorithm/GetScore.tsx
+++ src/Components/Algorithm/GetScore.tsx
@@ -3,11 +3,26 @@ import { suburbProps } from "../ExpandableComponent/ExpandableComponent";
 
 export const calcScore = (subs: suburbProps) => {
   const res = Math.round(
-    -subs.distance * 0.39 +
-      (600 - subs.medianRentPrice) * 0.47 +
-      +(subs.bus || subs.train || subs.tram) * 0.14
+    -subs.distance * 0.5 +
+      (800 - subs.medianRentPrice) * 0.47 +
+      +(subs.bus || subs.train || subs.tram) * 0.03
   );
-  return res > 100 ? 100 : res < 0 ? 0 : res;
+  console.log("this is res", res);
+  const convertStars = (score: number) => {
+    let finalStars = 1;
+    if (score > 100) {
+      finalStars = 5;
+    } else if (score > 80) {
+      finalStars = 4;
+    } else if (score > 60) {
+      finalStars = 3;
+    } else if (score > 40) {
+      finalStars = 2;
+    }
+    return finalStars;
+  };
+
+  return convertStars(res);
 };
 
 export const GetScore = (subs: suburbProps) => {
